83c42ade050a625a077833a21c4f02c10c66ef6d
Roman Sokolov <sokolov.r.v@gmail.com>
2011-10-28 06:16:06 +0400

    add common files, readme, etc.

---
+++ README.rst
@@ -0,0 +1,37 @@
+Dosca -- Dumb Simple Config Parser
+========
+
+About
+-----
+
+Just parses ini-style file and return dict with values. Really dumb.
+
+Features
+--------
+
+- Knows str, int, list, boolean and None types
+- Supports sections and subsections
+- Easy to use -- one function!
+
+FAQ
+---
+
+Q1: Does 'dosca' have validation capabilities?
+
+A1: No, it's not. If you want validate your config, use library designed for this task. Construct, Procrustes or, perhaps, Dumb Validation?
+
+
+Q2: Does it support interpolation or some complex types?
+
+A2: No, it's not. When I say 'dumb', I mean really dumb. Dosca only support basic features, essential for parsing config files.
+If you want advanced features, use ConfigObj or ConfigParser. Or xml and dtd?
+
+Install
+-------
+
+Not ready yet.
+
+License
+-------
+
+The MIT License, in LICENSE file.
---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import dosca
+
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist upload")
+    sys.exit()
+
+setup(
+    name='dosca',
+    version=dosca.__version__,
+    description='''Dumb Simple Config File Parser.''',
+    long_description=open('README.rst').read(),
+    author='Roma Sokolov',
+    author_email='sokolov.r.v@gmail.com',
+    url='https://github.com/little_arhat/dosca',
+    packages=[
+        'dosca'
+    ],
+    license='MIT',
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ),
+)
