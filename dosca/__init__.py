49bdad7570802def250288057f7536eaf77b305f
Roman Sokolov <sokolov.r.v@gmail.com>
2011-10-28 06:13:42 +0400

    rearrange package structure

---
+++ dosca/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+from .dosca import parse, parse_file, ParseError
+
+
+__all__ = ('parse', 'parse_file', 'ParseError')
+
+
+__version__ = '0.0.1'
+__author__ = 'Roma Sokolov'
+__license__ = 'MIT'
